1e42b04a549e0cb4e97abd3f7cba0f9e5a490ec6
viniciux <vinicius_q13@hotmail.com>
2022-09-17 14:22:44 -0300

    feat: terminei a estilização do video, e deixei a tela responsiva, tela esta toda responsiva

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-function toglleVideo(){
+function toggleVideo() {
     const trailer = document.querySelector('.trailer');
     const video = document.querySelector('video');
     trailer.classList.toggle('active');
 
-    video.curretTime = 0;
+    video.currentTime = 0;
     video.pause();
 }
